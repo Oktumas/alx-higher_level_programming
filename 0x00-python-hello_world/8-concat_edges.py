d649b0d4470064a59939c46210464d2d85527ea8
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-08-23 09:37:42 +0100

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,7 +1,5 @@
-tr = "Python is an interpreted, interactive, object-oriented programming\
-
-         language that combines remarkable power with very clear syntax"
-
-         str1 = str[39:67] + str[107:112] + str[0:6]
-
-         print(str1)
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str1 = str[39:67] + str[107:112] + str[0:6]
+print(str1)
