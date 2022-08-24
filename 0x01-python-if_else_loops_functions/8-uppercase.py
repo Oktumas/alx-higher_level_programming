719b273a0e3eec5ea5ac9da2d5e2c8891a62add8
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-08-24 23:49:30 +0100

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,8 @@
-se(str):
-
-        for i in range(len(str)):
-
-                    x = ord(str[i])
-
-                            if x >= 97 and x <= 122:
-
-                                            x = x - 32
-
-                                                    print("{}".format(chr(x)), end="")
-
-                                                        print()
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        x = ord(str[i])
+        if x >= 97 and x <= 122:
+            x = x - 32
+        print("{}".format(chr(x)), end="")
+    print()
